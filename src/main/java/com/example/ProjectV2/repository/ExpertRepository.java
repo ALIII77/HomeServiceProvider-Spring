45837594581c1a0b863bdcc90b95a2417f66311c
ALIII <alimoradi7am@gmail.com>
2022-12-20 19:45:53 +0330

    create repository classes

---
+++ src/main/java/com/example/ProjectV2/repository/ExpertRepository.java
@@ -0,0 +1,13 @@
+package com.example.ProjectV2.repository;
+
+import com.example.ProjectV2.entity.Expert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface ExpertRepository extends JpaRepository<Expert,Long> {
+
+}
+
