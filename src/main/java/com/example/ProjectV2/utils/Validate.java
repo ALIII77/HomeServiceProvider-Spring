c18a97e952322877a2dd0fa72e452624ad81a00f
ALIII <alimoradi7am@gmail.com>
2022-12-20 19:48:32 +0330

    create validation class

---
+++ src/main/java/com/example/ProjectV2/utils/Validate.java
@@ -0,0 +1,15 @@
+package com.example.ProjectV2.utils;
+
+import jakarta.validation.Validator;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.validation.Validation;
+import jakarta.validation.ValidatorFactory;
+
+public class Validate {
+    private static ValidatorFactory validatorFactory= Validation.buildDefaultValidatorFactory();
+
+    public static Validator getValidator(){
+        return validatorFactory.getValidator();
+    }
+}
