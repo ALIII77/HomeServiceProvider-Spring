6d18f1d69db44e0ab9cd8db43c8d6f769475bd5a
ALIII <alimoradi7am@gmail.com>
2022-12-27 16:28:16 +0330

    create CreditService and CreditServiceImpl class and add withdraw , deposit , payment by credit , payment by online method

---
+++ src/main/java/com/example/ProjectV2/service/CreditService.java
@@ -0,0 +1,18 @@
+package com.example.ProjectV2.service;
+
+import com.example.ProjectV2.entity.Credit;
+import com.example.ProjectV2.entity.Order;
+
+public interface CreditService {
+
+    void save(Credit credit);
+
+    void withdraw(Credit credit,double amount);
+
+    void deposit(Credit credit,double amount);
+
+    void credit(Long orderId , double amount);
+
+    void online (Long orderId , double amount);
+
+}
---
+++ src/main/java/com/example/ProjectV2/service/implementation/CreditServiceImpl.java
@@ -0,0 +1,88 @@
+package com.example.ProjectV2.service.implementation;
+
+
+import com.example.ProjectV2.entity.*;
+import com.example.ProjectV2.enums.OrderStatus;
+import com.example.ProjectV2.enums.TransactionType;
+import com.example.ProjectV2.exception.CustomizedIllegalArgumentException;
+import com.example.ProjectV2.exception.NotEnoughAmountException;
+import com.example.ProjectV2.exception.NotFoundException;
+import com.example.ProjectV2.repository.CreditRepository;
+import com.example.ProjectV2.service.CreditService;
+import com.example.ProjectV2.service.OrderService;
+import com.example.ProjectV2.service.TransactionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@org.springframework.stereotype.Service
+@RequiredArgsConstructor
+public class CreditServiceImpl implements CreditService {
+
+    private final CreditRepository creditRepository;
+    private final OrderService orderService;
+    private final TransactionService transactionService;
+
+
+    @Override
+    public void save(Credit credit) {
+        creditRepository.save(credit);
+    }
+
+
+    @Transactional
+    @Override
+    public void withdraw(@Valid Credit credit, double amount) {
+        if (credit.getAmount()>=amount){
+            credit.setAmount(credit.getAmount()-amount);
+            creditRepository.save(credit);
+        }else throw new NotEnoughAmountException("Not enough amount for withdraw");
+    }
+
+
+    @Transactional
+    @Override
+    public void deposit(@Valid Credit credit, double amount) {
+        credit.setAmount(credit.getAmount()+((amount*70)/100));
+        creditRepository.save(credit);
+    }
+
+    @Transactional
+    @Override
+    public void credit(Long orderId, double amount) {
+        Order findOrder = orderService.findOrderById(orderId).orElseThrow(() -> new NotFoundException("Not found Order ! "));
+        if (findOrder.getOrderStatus()!= OrderStatus.DONE){
+            throw new CustomizedIllegalArgumentException("Order must be DONE state !");
+        }
+        Expert findExpert = findOrder.getExpert();
+        Customer finCustomer = findOrder.getCustomer();
+        Credit sourceCredit = finCustomer.getCredit();
+        Credit destinationCredit = findExpert.getCredit();
+        withdraw(sourceCredit,amount);
+        deposit(destinationCredit,amount);
+        Transaction transaction = new Transaction(
+                findOrder.getCustomer().getCredit(),findOrder.getExpert().getCredit()
+                , amount,findOrder, TransactionType.CART_TO_CART
+        );
+        transactionService.save(transaction);
+    }
+
+
+    @Transactional
+    @Override
+    public void online(Long orderId, double amount){
+        Order findOrder = orderService.findOrderById(orderId).orElseThrow(() -> new NotFoundException("Not found Order ! "));
+        if (findOrder.getOrderStatus()!= OrderStatus.DONE){
+            throw new CustomizedIllegalArgumentException("Order must be DONE state !");
+        }
+        Expert findExpert = findOrder.getExpert();
+        Credit destinationCredit = findExpert.getCredit();
+        deposit(destinationCredit,amount);
+        Transaction transaction = new Transaction(
+                findOrder.getCustomer().getCredit(),findOrder.getExpert().getCredit()
+                , amount,findOrder, TransactionType.ONLINE
+        );
+        transactionService.save(transaction);
+    }
+
+}
