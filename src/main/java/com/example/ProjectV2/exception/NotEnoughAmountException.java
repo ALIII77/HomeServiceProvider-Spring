73ff4edcbaa3fbbd29ee7d8ac3dcef9b114921a5
ALIII <alimoradi7am@gmail.com>
2022-12-27 16:05:30 +0330

    add NotEnoughAmountException , NotUniqueException and TransactionException exception classes

---
+++ src/main/java/com/example/ProjectV2/exception/NotEnoughAmountException.java
@@ -0,0 +1,23 @@
+package com.example.ProjectV2.exception;
+
+public class NotEnoughAmountException extends RuntimeException{
+    public NotEnoughAmountException() {
+        super();
+    }
+
+    public NotEnoughAmountException(String message) {
+        super(message);
+    }
+
+    public NotEnoughAmountException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughAmountException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotEnoughAmountException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
