3e0252b70c9eccd48bab52f432326eccc5c6908b
ALIII <alimoradi7am@gmail.com>
2022-12-27 17:14:28 +0330

    update ExpertRepository.java

--- src/main/java/com/example/ProjectV2/repository/ExpertRepository.java
+++ src/main/java/com/example/ProjectV2/repository/ExpertRepository.java
@@ -1,6 +1,7 @@
 package com.example.ProjectV2.repository;
 
 import com.example.ProjectV2.entity.Expert;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface ExpertRepository extends JpaRepository<Expert, Long> {
+public interface ExpertRepository extends JpaRepository<Expert, Long> , Specification {
 
     Optional<Expert> findExpertByUsername(String username);
 
