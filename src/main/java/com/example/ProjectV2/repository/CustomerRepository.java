cda5db6cfb4ee35196c357a929fe5fdebdbda846
ALIII <alimoradi7am@gmail.com>
2022-12-27 16:07:17 +0330

    add findBooksByAuthorNameAndTitle method in CustomerRepository.java

--- src/main/java/com/example/ProjectV2/repository/CustomerRepository.java
+++ src/main/java/com/example/ProjectV2/repository/CustomerRepository.java
@@ -1,17 +1,30 @@
 package com.example.ProjectV2.repository;
 
 import com.example.ProjectV2.entity.Customer;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> , JpaSpecificationExecutor<Customer> {
 
 
     Optional<Customer> findCustomerByUsername(String username);
+
+
+
+    List<Customer> findBooksByAuthorNameAndTitle(Map<String,String> map);
+
+
 
 
 }
