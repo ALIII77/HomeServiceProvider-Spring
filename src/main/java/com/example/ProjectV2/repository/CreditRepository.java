7146cc23e7485a39e93bb93c96b4538b62b736e7
ALIII <alimoradi7am@gmail.com>
2022-12-27 16:06:10 +0330

    create CreditRepository.java

---
+++ src/main/java/com/example/ProjectV2/repository/CreditRepository.java
@@ -0,0 +1,7 @@
+package com.example.ProjectV2.repository;
+
+import com.example.ProjectV2.entity.Credit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreditRepository extends JpaRepository<Credit,Long> {
+}
