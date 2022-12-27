1b26e4aacfe067d179340879dfc4255219f49422
ALIII <alimoradi7am@gmail.com>
2022-12-27 15:25:21 +0330

    add NotEnoughAmount and NotUniqueException exception to GlobalMvcExceptionHandler

--- src/main/java/com/example/ProjectV2/config/GlobalMvcExceptionHandler.java
+++ src/main/java/com/example/ProjectV2/config/GlobalMvcExceptionHandler.java
@@ -1,9 +1,6 @@
 package com.example.ProjectV2.config;
 
-import com.example.ProjectV2.exception.CustomizedIllegalArgumentException;
-import com.example.ProjectV2.exception.ImageException;
-import com.example.ProjectV2.exception.NotFoundException;
-import com.example.ProjectV2.exception.PermissionDeniedException;
+import com.example.ProjectV2.exception.*;
 import com.sun.jdi.request.ExceptionRequest;
 import jakarta.persistence.NoResultException;
 import org.springframework.http.HttpStatus;
@@ -18,40 +15,42 @@ public class GlobalMvcExceptionHandler {
 
     @ExceptionHandler(value = RuntimeException.class)
     public ResponseEntity<Object> handleRuntimeException(Exception ex, WebRequest request) {
-        // 1. status --> BAD REQUEST
-        // 2. return exception message to user
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
 
     @ExceptionHandler(value = NotFoundException.class)
     public ResponseEntity<Object> handleNotFoundException(NotFoundException ex, WebRequest request) {
-        // 1. status --> BAD REQUEST
-        // 2. return exception message to user
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
 
     @ExceptionHandler(value = CustomizedIllegalArgumentException.class)
     public ResponseEntity<Object> handleNotFoundException(CustomizedIllegalArgumentException ex, WebRequest request) {
-        // 1. status --> BAD REQUEST
-        // 2. return exception message to user
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
 
     @ExceptionHandler(value = ImageException.class)
     public ResponseEntity<Object> handleNotFoundException(ImageException ex, WebRequest request) {
-        // 1. status --> BAD REQUEST
-        // 2. return exception message to user
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ex.getMessage());
     }
 
 
     @ExceptionHandler(value = PermissionDeniedException.class)
     public ResponseEntity<Object> handleNotFoundException(PermissionDeniedException ex, WebRequest request) {
-        // 1. status --> BAD REQUEST
-        // 2. return exception message to user
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+
+    @ExceptionHandler(value = NotEnoughAmount.class)
+    public ResponseEntity<Object> handleNotFoundException(NotEnoughAmount ex, WebRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+
+    @ExceptionHandler(value = NotUniqueException.class)
+    public ResponseEntity<Object> handleNotFoundException(NotUniqueException ex, WebRequest request) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
