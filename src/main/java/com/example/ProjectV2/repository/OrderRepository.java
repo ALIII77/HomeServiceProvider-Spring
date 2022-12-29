e290db3277e891f2bd4d34a201e0deb9a7e933d5
ALIII <alimoradi7am@gmail.com>
2022-12-29 22:53:31 +0330

    add method showAllOrderByExpertSubService add method findAllByOrderStatus to add OrderService.java

--- src/main/java/com/example/ProjectV2/repository/OrderRepository.java
+++ src/main/java/com/example/ProjectV2/repository/OrderRepository.java
@@ -1,16 +1,27 @@
 package com.example.ProjectV2.repository;
 
 import com.example.ProjectV2.entity.Order;
+import com.example.ProjectV2.entity.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+
+
+    @Query("""
+select o from Order as o join SubService as ss on o.subService=ss join Expert as e where e.id=:expertId
+""")
+    List<Order> showAllOrderByExpertSubService(Long expertId);
+
+
+    List<Order> findAllByOrderStatus(OrderStatus orderStatus);
+
+
 
 
 }
--- src/main/java/com/example/ProjectV2/repository/ServiceRepository.java
+++ src/main/java/com/example/ProjectV2/repository/ServiceRepository.java
@@ -16,4 +16,5 @@ public interface ServiceRepository extends JpaRepository<Service, Long> {
     List<Service> findAll();
 
 
+
 }
