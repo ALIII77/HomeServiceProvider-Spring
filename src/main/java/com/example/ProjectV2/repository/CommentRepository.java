3f348dadf492e4798179f91e43bc9a753b149460
ALIII <alimoradi7am@gmail.com>
2022-12-29 22:49:32 +0330

    add findScoreByExpertId.java to CommentRepository.java

--- src/main/java/com/example/ProjectV2/repository/CommentRepository.java
+++ src/main/java/com/example/ProjectV2/repository/CommentRepository.java
@@ -2,11 +2,18 @@ package com.example.ProjectV2.repository;
 
 import com.example.ProjectV2.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+
+    @Query("select c.score from Comment as c where c.expert.id=:expertId")
+    List<double> findScoreByExpertId(Long expertId);
 
 
 }
