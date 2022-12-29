c180b3f244f60d1044dfd438239bab9189abf76a
ALIII <alimoradi7am@gmail.com>
2022-12-29 23:04:37 +0330

    update method add method show all customer delete customer search customer credit online credit service

--- src/main/java/com/example/ProjectV2/service/implementation/CustomerServiceImpl.java
+++ src/main/java/com/example/ProjectV2/service/implementation/CustomerServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.ProjectV2.service.implementation;
 
 import com.example.ProjectV2.entity.*;
-import com.example.ProjectV2.exception.CustomizedIllegalArgumentException;
 import com.example.ProjectV2.exception.NotFoundException;
 import com.example.ProjectV2.exception.NotUniqueException;
 import com.example.ProjectV2.exception.PermissionDeniedException;
@@ -10,10 +9,6 @@ import com.example.ProjectV2.service.*;
 
 import java.util.*;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -23,22 +18,21 @@ import org.springframework.transaction.annotation.Transactional;
 
 @org.springframework.stereotype.Service
 public class CustomerServiceImpl implements CustomerService {
-    @Autowired
-    ApplicationContext applicationContext;
+
+    private final ApplicationContext applicationContext;
     private final CustomerRepository customerRepository;
     private final ServiceService serviceService;
     private final SubServiceService subServiceService;
-    private final TransactionService transactionService;
-    private final CreditService creditService;
+
 
     @Autowired
-    public CustomerServiceImpl(CustomerRepository customerRepository
-            , ServiceService serviceService, SubServiceService subServiceService, TransactionService transactionService, CreditService creditService) {
+    public CustomerServiceImpl(ApplicationContext applicationContext, CustomerRepository customerRepository
+            , ServiceService serviceService, SubServiceService subServiceService, TransactionService transactionService) {
+        this.applicationContext = applicationContext;
         this.customerRepository = customerRepository;
         this.serviceService = serviceService;
         this.subServiceService = subServiceService;
-        this.transactionService = transactionService;
-        this.creditService = creditService;
+
     }
 
 
@@ -46,6 +40,8 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public Customer save(@Valid Customer customer) {
         if (checkUsername(customer.getUsername())) {
+            Credit credit = new Credit();
+            customer.setCredit(credit);
             return customerRepository.save(customer);
         }
         throw new NotUniqueException("customer username is exists");
@@ -55,24 +51,18 @@ public class CustomerServiceImpl implements CustomerService {
     @Transactional
     @Override
     public void changePassword(Customer customer, String newPassword) {
-        Optional<Customer> customerOptional = customerRepository.findCustomerByUsername(customer.getUsername());
-        if (customerOptional.isEmpty()) {
-            throw new NotFoundException("Not found customer to change password");
-        }
-        if (!customer.getPassword().equals(customerOptional.get().getPassword())) {
+        Customer findCustomer = customerRepository.findCustomerByUsername(customer.getUsername())
+                .orElseThrow(() -> new NotFoundException("Not found customer to change password"));
+        if (!customer.getPassword().equals(findCustomer.getPassword())) {
             throw new PermissionDeniedException("password incorrect");
         }
         System.out.println("Customer Find!");
-        Customer findCustomer = customerOptional.get();
         if ((findCustomer.getPassword().equals(newPassword))) {
             throw new PermissionDeniedException("Enter new password that not equal with old password");
         }
         findCustomer.setPassword(newPassword);
-        try {
-            customerRepository.save(findCustomer);
-        } catch (CustomizedIllegalArgumentException exception) {
-            System.out.println(exception.getMessage());
-        }
+        customerRepository.save(findCustomer);
+
     }
 
 
@@ -120,11 +110,54 @@ public class CustomerServiceImpl implements CustomerService {
     @Transactional
     @Override
     public void cartToCartPaymentWages(Long orderId, double amount) {
-        creditService.credit(orderId, amount);
+        creditService().credit(orderId, amount);
     }
 
     public void onlinePayment(Long orderId, double amount) {
-        creditService.online(orderId, amount);
+        creditService().online(orderId, amount);
     }
+
+    @Override
+    public List<Customer> showAllCustomer() {
+        return customerRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public void deleteCustomer(String username) {
+        customerRepository.deleteCustomerByUsername(username);
+    }
+
+
+    @Override
+    @Transactional
+    public List<Customer> searchCustomer(Map<String, String> predicateMap) {
+        return customerRepository.findAll(returnSpecification(predicateMap));
+    }
+    Specification<Customer> returnSpecification(Map<String, String> predicateMap) {
+        Specification<Customer> specification = Specification.where(null);
+        for (Map.Entry<String, String> entry : predicateMap.entrySet()) {
+            specification=specification.and((expert, cq, cb) -> cb.equal(expert.get(entry.getKey()), entry.getValue()));
+        }
+        return specification;
+    }
+
+
+    @Override
+    public void credit(Long orderId, double amount) {
+        creditService().credit(orderId, amount);
+    }
+
+
+    @Override
+    public void online(Long orderId, double amount) {
+        creditService().online(orderId, amount);
+    }
+
+
+    private CreditService creditService() {
+        return applicationContext.getBean(CreditService.class);
+    }
+
 
 }
