26ea342c7ef4a2a071f51bb20edfe3f51b518384
ALIII <alimoradi7am@gmail.com>
2022-12-29 22:51:14 +0330

    update ExpertRepository.java

--- src/main/java/com/example/ProjectV2/repository/ExpertRepository.java
+++ src/main/java/com/example/ProjectV2/repository/ExpertRepository.java
@@ -1,19 +1,27 @@
 package com.example.ProjectV2.repository;
 
+import com.example.ProjectV2.entity.Customer;
 import com.example.ProjectV2.entity.Expert;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
-public interface ExpertRepository extends JpaRepository<Expert, Long> , Specification {
+public interface ExpertRepository extends JpaRepository<Expert, Long>, JpaSpecificationExecutor<Expert> {
 
     Optional<Expert> findExpertByUsername(String username);
 
-    Expert findExpertById(Long id);
+    void deleteExpertByUsername(String username);
+
+
+
+
 
 
 }
