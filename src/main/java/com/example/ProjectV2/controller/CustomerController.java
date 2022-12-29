b2f0edf528795daee1c52eaf9a23e728f5188902
ALIII <alimoradi7am@gmail.com>
2022-12-29 22:26:28 +0330

    add some method in CustomerController.java

--- src/main/java/com/example/ProjectV2/controller/CustomerController.java
+++ src/main/java/com/example/ProjectV2/controller/CustomerController.java
@@ -6,7 +6,6 @@ import com.example.ProjectV2.service.*;
 import lombok.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +13,6 @@ import java.util.List;
 @RequestMapping("/customer")
 @RequiredArgsConstructor
 public class CustomerController {
-
     public final CustomerService customerService;
     public final CommentService commentService;
     public final ServiceService serviceService;
@@ -24,6 +22,12 @@ public class CustomerController {
     public final ExpertService expertService;
 
 
+    @PostMapping("save-customer")
+    public void save(@RequestBody Customer customer) {
+        customerService.save(customer);
+    }
+
+
     @PutMapping("change-password-customer")
     public void changePassword(@RequestBody CustomerChangePasswordDto changePasswordDTO) {
         customerService.changePassword(changePasswordDTO.getCustomer(), changePasswordDTO.getNewPassword());
@@ -31,13 +35,30 @@ public class CustomerController {
 
 
     @GetMapping("show-all-Services")
-    public List<Service> showAllServices() {
-        return serviceService.findAllServices();
+    public List<ServiceDto> showAllServices() {
+        List<ServiceDto> serviceList = new ArrayList<>();
+        for (Service s : serviceService.findAllServices()) {
+            ServiceDto serviceDto = new ServiceDto();
+            serviceDto.setName(s.getName());
+            serviceList.add(serviceDto);
+        }
+        return serviceList;
+
     }
 
 
-    @GetMapping("show-all-sub-service-with-service-name")
-    public List<SubServiceDto> showAllSubServiceWithServiceName(@RequestParam String serviceName) {
+    @GetMapping("show-service/{serviceName}")
+    public ServiceDto showService(@PathVariable String serviceName) {
+        Service service = serviceService.findServiceByName(serviceName).get();
+        ServiceDto serviceDto = new ServiceDto();
+        serviceDto.setName(service.getName());
+        serviceDto.setId(service.getId());
+        return serviceDto;
+    }
+
+
+    @GetMapping("show-all-sub-service-with-service-name/{serviceName}")
+    public List<SubServiceDto> showAllSubServiceWithServiceName(@PathVariable String serviceName) {
         Service service = new Service();
         service.setName(serviceName);
         List<SubService> subServicesByServiceName = subServiceService.findAllSubServicesByService(service);
@@ -51,19 +72,31 @@ public class CustomerController {
     }
 
 
+    @PostMapping("add-order")
+    public void addOrder(@RequestBody OrderDto orderDTO) {
+        orderService.addOrder(orderDTO.getOrder(), orderDTO.getCustomerId(), orderDTO.getSubServiceName());
+
+    }
+
+
+    @PutMapping("select-expert")
+    public void selectExpert(@RequestBody SelectExpertDto selectExpert) {
+        expertService.selectExpert(selectExpert.getOfferId(), selectExpert.getCustomerId());
+    }
+
+
     @PostMapping("add-comment")
     public void addComment(@RequestBody AddCommentByCustomerDto commentDTO) {
         Comment newComment = new Comment();
         newComment.setText(commentDTO.getText());
         newComment.setScore(commentDTO.getScore());
-        commentService.addComment(newComment, commentDTO.getOrderId());
-
+        commentService.addComment(newComment, commentDTO.getExpertUsername(), commentDTO.getOrderId());
     }
 
 
     @GetMapping("offers-sorted-by-price")
-    public List<SortedOffersDto> sortedOffersByPrices(@RequestParam Long orderId) {                             //check
-        List<Offer> offerList = offerService.findAllOfferOneOrderByExpertScore(orderId);
+    public List<SortedOffersDto> sortedOffersByPrices(@RequestParam Long orderId) {
+        List<Offer> offerList = offerService.findAllOfferOneOrderByPrice(orderId);
         List<SortedOffersDto> resultSortedOffer = new ArrayList<>();
         for (Offer o : offerList) {
             SortedOffersDto offersSortedByPrice
@@ -76,8 +109,8 @@ public class CustomerController {
 
 
     @GetMapping("offers-sorted-by-expert-score")
-    public List<SortedOffersDto> sortedOffersByExpertScore(@RequestParam Long orderId, Long expertId) {          //check
-        List<Offer> offerList = offerService.findOfferByOrderIdAndExpertId(orderId, expertId).stream().toList();
+    public List<SortedOffersDto> sortedOffersByExpertScore(@RequestParam Long orderId) {
+        List<Offer> offerList = offerService.findAllOfferOneOrderByExpertScore(orderId);
         List<SortedOffersDto> resultSortedOffer = new ArrayList<>();
         for (Offer o : offerList) {
             SortedOffersDto offerSortedByExpertScore
@@ -89,35 +122,33 @@ public class CustomerController {
     }
 
 
-    @PutMapping("add-order")
-    public void addOrder(@RequestBody OrderDto orderDTO) {
-        orderService.addOrder(orderDTO.getOrder(), orderDTO.getCustomerId(), orderDTO.getSubServiceName());
-
-    }
-
-
-    @PutMapping("select-expert")
-    public void selectExpert(@RequestBody SelectExpertDto selectExpert) {
-        expertService.selectExpert(selectExpert.getOfferId(), selectExpert.getOrderId());
-    }
-
-
-    @PutMapping("change-order-status-to-started")
-    public void changeOrderStatusToStarted(@RequestParam Long orderId) {                                        //check
+    @PutMapping("change-order-status-to-started/{orderId}")
+    public void changeOrderStatusToStarted(@PathVariable Long orderId) {
         Order order = new Order();
         order.setId(orderId);
         orderService.changeOrderStatusToStarted(order);
     }
 
 
-    @PutMapping("change-order-status-to-done")
-    public void changeOrderStatusToDone(@RequestParam Long orderId) {                               //check
+    @PutMapping("change-order-status-to-done/{orderId}")
+    public void changeOrderStatusToDone(@PathVariable Long orderId) {
         Order order = new Order();
         order.setId(orderId);
         orderService.changeOrderStatusToDone(order);
     }
 
 
+    @PutMapping("pay-credit")
+    public void payWithCredit(@RequestBody PayDto payDto) {
+        customerService.credit(payDto.getOrderId(), payDto.getAmount());
+
+    }
+
+
+    @PutMapping("pay-online")
+    public void payWithOnline(@RequestBody PayDto payDto) {
+        customerService.online(payDto.getOrderId(), payDto.getAmount());
+    }
 
 
 }
