95b99c8420ab4dfc583f87c200d4179b1ebf9f21
ALIII <alimoradi7am@gmail.com>
2023-01-12 23:13:56 +0330

    add config class

--- src/main/java/com/example/ProjectV2/config/Config.java
+++ src/main/java/com/example/ProjectV2/config/Config.java
@@ -1,21 +1,28 @@
 package com.example.ProjectV2.config;
 
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class Config {
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    public PasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public JavaMailSender javaMailSender(){
         return new JavaMailSenderImpl();
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 }
