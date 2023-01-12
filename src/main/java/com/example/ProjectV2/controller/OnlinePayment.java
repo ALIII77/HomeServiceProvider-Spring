71ab85260cdab1d4568fc076970674381d5f04d9
ALIII <alimoradi7am@gmail.com>
2023-01-12 23:27:21 +0330

    create OnlinePayment.java

---
+++ src/main/java/com/example/ProjectV2/controller/OnlinePayment.java
@@ -0,0 +1,22 @@
+package com.example.ProjectV2.controller;
+
+
+import com.example.ProjectV2.utils.SendEmail;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequiredArgsConstructor
+public class OnlinePayment {
+
+    private final SendEmail sendEmail;
+
+    @RequestMapping("/online-payment")
+    public ModelAndView paymentOnline (){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
+    }
+}
